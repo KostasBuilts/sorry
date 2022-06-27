d1eaaad6063d7aa037b65c463e582b7b84df40b8
KostasBuilts <78872425+KostasBuilts@users.noreply.github.com>
2022-06-27 19:26:01 +0300

    Update Sorry.c

--- Sorry.c
+++ Sorry.c
@@ -4,11 +4,11 @@ int main() {
     char UsrIn[3];
     printf("Did I eat your souvlaki (y/n):");
     scanf("%c", UsrIn);
-    if(UsrIn == "y")
+    if( strcmp (UsrIn, "y") == 0 || strcmp (UsrIn, "yes") == 0)
     {
         printf("I am terribly sorry");
     }
-    if(UsrIn == "n" || UsrIn == "no")
+    if(strcmp (UsrIn, "n") == 0|| strcmp (UsrIn, "no") == 0)
     {
         printf("ok");
     }
