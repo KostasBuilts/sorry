8df2046807ae0b80fa0b992bec25bf690844c41f
KostasBuilts <78872425+KostasBuilts@users.noreply.github.com>
2023-07-19 10:53:28 +0300

    Update Sorry.asm

--- Sorry.asm
+++ Sorry.asm
@@ -1,36 +1,58 @@
-; to be continued
+section .data
+    prompt db 'Did I eat your souvlaki? (y/n): '
+    prompt_len equ $ - prompt
+    hello db 'I am terribly sorry'
+    hello_len equ $ - hello
+    bye db 'ok'
+    bye_len equ $ - bye
 
-section .data                           
-   startMsg db 'Did I eat your souvlaki (y/n):' 
-   lenstartMsg equ $-startMsg
-   sorryMsg db 'I am terribly sorry'
-   lensorryMsg equ $-sorryMsg                          
+section .text
+    global _start
 
-section .bss           
-   ansr resb 5
-	
-section .text          
-   global _start
-	
-_start:                
-   mov eax, 4
-   mov ebx, 1
-   mov ecx, startMsg
-   mov edx, lenstartMsg
-   int 80h
-  
-   mov eax, 3
-   mov ebx, 2
-   mov ecx, ansr  
-   mov edx, 5          
-   int 80h 
+_start:
+    ; Print prompt
+    mov eax, 4          ; system call for write
+    mov ebx, 1          ; file descriptor for stdout
+    mov ecx, prompt     ; message to print
+    mov edx, prompt_len ; message length
+    int 0x80            ; call kernel
 
-   mov eax, 4
-   mov ebx, 1
-   mov ecx, sorryMsg
-   mov edx, lensorryMsg
-   int 80h
-    
-   mov eax, 1
-   mov ebx, 0
-   int 80h
+    ; Read character from keyboard
+    mov eax, 3          ; system call for read
+    mov ebx, 0          ; file descriptor for stdin
+    mov ecx, input      ; buffer for input
+    mov edx, 1          ; number of bytes to read
+    int 0x80            ; call kernel
+
+    cmp byte [input], 'y'
+    je say_hello
+    cmp byte [input], 'n'
+    je say_bye
+    jmp _start
+
+say_hello:
+    ; Print "Hello!" message
+    mov eax, 4          ; system call for write
+    mov ebx, 1          ; file descriptor for stdout
+    mov ecx, hello      ; message to print
+    mov edx, hello_len  ; message length
+    int 0x80            ; call kernel
+    jmp exit
+
+say_bye:
+    ; Print "Bye!" message
+    mov eax, 4          ; system call for write
+    mov ebx, 1          ; file descriptor for stdout
+    mov ecx, bye        ; message to print
+    mov edx, bye_len    ; message length
+    int 0x80            ; call kernel
+    jmp exit
+
+exit:
+    ; Exit program
+    mov eax, 1          ; system call for exit
+    xor ebx, ebx        ; return value of 0
+    int 0x80            ; call kernel
+
+section .bss
+    input resb 1        ; buffer for input
